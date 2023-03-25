a05bb3f8ab682e2c225612d6d10e2dd3f286736a
4l3x777 <github.com/4l3x777>
2023-03-25 17:54:10 +0300

    Release version.

---
+++ print_ip.h
@@ -0,0 +1,138 @@
+/*!
+    \file
+    \brief Header file
+
+    That file contain main class and helper namespace with some featches
+*/
+
+#pragma once
+#include <iostream>
+#include <type_traits>
+#include <list>
+#include <vector>
+#include <tuple>
+#include <string>
+
+using namespace std;
+
+/*!
+	\brief Helper namespace
+    \version 1.0
+    \author 4l3x77
+
+	That namespace contain some featches for print_ip class.
+    
+    \defgroup print_ip_helper
+    @{
+*/
+namespace print_ip_helper { 
+    /*!
+        \brief Helper variable. Determines ip address octets's delimeter.
+    */
+    static const char ip_delimeter = '.';
+    /*!
+        \brief Helper structs. Determines template type is vector or list.
+        \defgroup is_vector_or_list
+        @{
+    */
+    template <typename>
+    struct is_vector_or_list : false_type {};
+
+    template <typename T, typename... Args>
+    struct is_vector_or_list<std::vector<T, Args...>> : true_type {};
+
+    template <typename T, typename... Args>
+    struct is_vector_or_list<std::list<T, Args...>> : true_type {};
+    /*! @} */
+
+    /**
+        \brief Helper structs for printing ip addresses as tuples of same types.
+        \defgroup helper_print_tuple
+        @{
+    */
+    template <size_t typle_size>
+    struct helper_print_tuple
+    {
+        template <typename element0, typename... tail>
+        static constexpr void print(const tuple<element0, tail...>& tuple) {
+            auto element = get<sizeof...(tail) + 1 - typle_size>(tuple);
+            static_assert(is_same_v<element0, decltype(element)>, "Different element's types in tuple!");
+            cout << element;
+            if (typle_size > 1) cout << ip_delimeter;
+            helper_print_tuple<typle_size - 1>::print(tuple);
+        }
+    };
+
+    template <>
+    struct helper_print_tuple<0>
+    {
+        template <typename... tail>
+        static constexpr void print(const tuple<tail...>&) {}
+    };
+    /*! @} */
+}
+/*! @} */
+
+
+/*!
+	\brief Main class.
+    \version 1.0
+    \author 4l3x77
+
+	That class contain print_ip constructors for release metaprogramming SFINAE technique.
+
+    \defgroup print_ip
+    @{
+*/
+class print_ip {
+public:
+    /*!
+        \brief Prints ip address represented as an integral type.
+        \param value is any integral value
+        \returns print ip to stdout
+    */
+    template <typename T, typename std::enable_if<is_integral_v<T>,bool>::type = true>
+    print_ip(T value) {
+        static const size_t constexpr number_of_octets = sizeof(T);
+        for (size_t i = number_of_octets; i--;) {
+            cout << (value >> (i << 3) & 0xff);
+            if (i > 0) cout << print_ip_helper::ip_delimeter;
+        }
+        cout << endl;
+    }
+
+    /*!
+        \brief Prints ip address represented as a string.
+        \param value is reference to string
+        \returns print ip to stdout
+    */
+    print_ip(const string& value) {
+        cout << value << endl;
+    }
+
+    /*!
+        \brief Prints ip address represented as vector or list.
+        \param container is reference to vector or list
+        \returns print ip to stdout
+    */
+    template<class T, typename std::enable_if<print_ip_helper::is_vector_or_list<T>{},bool>::type = true>
+    print_ip(const T& container) {
+        for (auto it = container.begin(); it != container.end(); ++it) {
+            cout << *it;
+            if (it != prev(container.end())) cout << print_ip_helper::ip_delimeter;
+        }
+        cout << endl;
+    }
+
+    /*!
+        \brief Prints ip address represented as tuple.
+        \param container is reference to tuple with determine all octet's type
+        \returns print ip to stdou
+    */
+    template <typename... tail>
+    print_ip(const tuple<tail...>& tuple) {
+        print_ip_helper::helper_print_tuple<sizeof...(tail)>::print(tuple);
+        cout << endl;
+    }
+};
+/*! @} */
---
+++ test_print_ip_gtest.cpp
@@ -0,0 +1,75 @@
+#include "gtest/gtest.h"
+#include "print_ip.h"
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class TestPrintIp : public ::testing::Test {
+	streambuf* old_stdout{nullptr};
+
+protected:
+	void SetUp() {
+		old_stdout = std::cout.rdbuf(new_stdout.rdbuf());
+	}
+
+	void TearDown() {
+		old_stdout = std::cout.rdbuf(old_stdout);
+	}
+
+	stringstream new_stdout;
+};
+
+TEST_F(TestPrintIp, test_int8_t)
+{
+	print_ip( int8_t{-1} ); // 255
+	ASSERT_EQ(new_stdout.str(), string("255\n"));
+}
+
+TEST_F(TestPrintIp, test_int16_t)
+{
+	print_ip( int16_t{0} ); // 0.0
+	ASSERT_EQ(new_stdout.str(), string("0.0\n"));
+}
+
+TEST_F(TestPrintIp, test_int32_t)
+{
+	print_ip( int32_t{2130706433} ); // 127.0.0.1
+	ASSERT_EQ(new_stdout.str(), string("127.0.0.1\n"));
+}
+
+TEST_F(TestPrintIp, test_int64_t)
+{
+	print_ip( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41
+	ASSERT_EQ(new_stdout.str(), "123.45.67.89.101.112.131.41\n");
+}
+
+TEST_F(TestPrintIp, test_string)
+{
+	print_ip( std::string{"Hello, World!"} ); // Hello, World!
+	ASSERT_EQ(new_stdout.str(), "Hello, World!\n");
+}
+
+TEST_F(TestPrintIp, test_vector)
+{
+	print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400
+	ASSERT_EQ(new_stdout.str(), "100.200.300.400\n");
+}
+
+TEST_F(TestPrintIp, test_list)
+{
+	print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100
+	ASSERT_EQ(new_stdout.str(), "400.300.200.100\n");
+}
+
+TEST_F(TestPrintIp, test_tuple)
+{
+	print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+	ASSERT_EQ(new_stdout.str(), "123.456.789.0\n");
+}
+
+int main(int argc, char *argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
